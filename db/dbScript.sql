402c8013dff03b0637f6ba85d66fcb1175c3dbef
Raemskii <raemskii14@gmail.com>
2023-02-17 19:41:22 +0100

    updated script

--- db/dbScript.sql
+++ db/dbScript.sql
@@ -2,5 +2,5 @@ drop database if exists scrum_coaching;
 create database scrum_coaching;
 use scrum_coaching;
 
-drop table if exists scrumcoach;
-create table scrumcoach (id int not null primary key, username varchar(16) not null, name varchar(32) not null, password varchar(32) not null, timestamp datetime);
+drop table if exists users;
+create table users (id int not null primary key, username varchar(16) not null, password varchar(32) not null, email varchar(32) not null, timestamp datetime);
