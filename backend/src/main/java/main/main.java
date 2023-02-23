5faadcda2b4f7f4853b1294bcc67c2f13af54349
sZellwegerGit <s.zellweger@me.com>
2023-02-23 22:31:06 +0100

    main ?

--- backend/src/main/java/main/main.java
+++ backend/src/main/java/main/main.java
@@ -2,15 +2,13 @@ package main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
-@RestController
+
 @SpringBootApplication
-public class main {
+public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		SpringApplication.run(main.class, args);
-
+		SpringApplication.run(Main.class, args);
 	}
 
 }
