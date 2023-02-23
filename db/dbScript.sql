aff21bc6544b525f1b13f8e3172ac0edf365db9a
aco293 <aleks_amidzic@hotmail.com>
2023-02-23 22:13:23 +0100

    Revert "Merge branch 'main' of https://github.com/sZellwegerGit/ScrumCoaching"
    This reverts commit 22b4985a2de208daf01f23abd2b39f230353ccf8, reversing
    changes made to 189ae23f303b981cbf442e0c7e4ff9e6ecdf5d0c.

--- db/dbScript.sql
+++ db/dbScript.sql
@@ -3,44 +3,4 @@ create database scrum_coaching;
 use scrum_coaching;
 
 drop table if exists users;
-create table users (
-    id int not null primary key, 
-    username varchar(16) not null, 
-    password varchar(32) not null, 
-    email varchar(32) not null, 
-    timestamp datetime
-);
-
-insert into users values (
-    1,
-    "user1",
-    "user",
-    "user@user.com",
-    CURRENT_TIMESTAMP()
-);
-
-insert into users values (
-    2,
-    "user2",
-    "user",
-    "user@user.com",
-    CURRENT_TIMESTAMP()
-);
-
-insert into users values (
-    3,
-    "user3",
-    "user",
-    "user@user.com",
-    CURRENT_TIMESTAMP()
-);
-
-drop table if exists userRating;
-create table userRating (
-    id int not null primary key,
-    userId int not null,
-    reliability int not null default 3,
-    efficiency int not null default 3,
-    teamplayer int not null default 3
-);
-
+create table users (id int not null primary key, username varchar(16) not null, password varchar(32) not null, email varchar(32) not null, timestamp datetime);
